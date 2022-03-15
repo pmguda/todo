63960a83a1cceb94dce0d7fd8d1cc5db91e815ff
이성구 <ganziguda@gmail.com>
2022-03-15 09:28:09 +0900

    CORS 설정 추가

--- src/main/java/com/pmguda/todo/controller/TodoController.java
+++ src/main/java/com/pmguda/todo/controller/TodoController.java
@@ -1,15 +1,22 @@
 package com.pmguda.todo.controller;
 
 import com.pmguda.todo.dto.ResponseDTO;
+import com.pmguda.todo.dto.TodoDTO;
+import com.pmguda.todo.model.TodoEntity;
 import com.pmguda.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //http와 관련된 코드 및 요청/응답 매핑을 스프링이 처리
 @RequestMapping("todo") //리소스 경로
@@ -25,5 +32,82 @@ public class TodoController {
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
     }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user"; // temporary user id
+
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            entity.setId(null);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response =  ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temporaryUserId = "temporary-user";
+
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        ResponseDTO<TodoDTO> respose = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        return ResponseEntity.ok().body(respose);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> UpdateTodo(@RequestBody TodoDTO dto) {
+        String temporaryUserId = "temporary-user";
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        entity.setUserId(temporaryUserId);
+
+        List<TodoEntity> entities = service.update(entity);
+
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> DeleteTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.delete(entity);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+
+    }
+
 
 }
--- src/main/java/com/pmguda/todo/service/TodoService.java
+++ src/main/java/com/pmguda/todo/service/TodoService.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Slf4j  //lombok 로그 어노테이션 사용
 @Service
 public class TodoService {
@@ -21,5 +24,59 @@ public class TodoService {
         TodoEntity saveEntity = repository.findById(entity.getId()).get();
         log.info("saveEntity :" + saveEntity);
         return saveEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id : {} is saved.", entity.getId());
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("error deleting entity ", entity.getId(), e);
+
+            throw new RuntimeException("error deleting entity " + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+
     }
 }
