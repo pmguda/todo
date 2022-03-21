8b7b3ec5dd85cf832ece120cb78c8282ab6e11a7
이성구 <ganziguda@gmail.com>
2022-03-21 11:39:29 +0900

    Spring Scheduler 테스트

--- src/main/java/com/pmguda/todo/TodoRestapiApplication.java
+++ src/main/java/com/pmguda/todo/TodoRestapiApplication.java
@@ -2,8 +2,11 @@ package com.pmguda.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class TodoRestapiApplication {
 
 	public static void main(String[] args) {
